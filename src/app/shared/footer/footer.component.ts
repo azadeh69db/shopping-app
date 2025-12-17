ef9e84f8ea050cf1993cad91cc73aa9410755553
azadeh dehgan <azadeh69db@gmail.com>
2025-12-17 21:54:06 +0330

    ok

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [MatInputModule],
+  imports: [MatInputModule,MatIconModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
