2a42f4301bd13dff5b283815611aa7454ec5bbbc
azadeh dehgan <azadeh69db@gmail.com>
2025-12-18 16:07:42 +0330

    ok

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -3,11 +3,12 @@ import { BillingComponent } from './components/billing/billing.component';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { PaypalformComponent } from './components/paypalform/paypalform.component';
 @Component({
   selector: 'app-checkout',
   standalone: true,
   imports: [BillingComponent,RouterOutlet
-    ,HeaderComponent,FooterComponent],
+    ,HeaderComponent,FooterComponent,PaypalformComponent],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
--- src/app/pages/checkout/components/paypalform/paypalform.component.ts
+++ src/app/pages/checkout/components/paypalform/paypalform.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
 @Component({
   selector: 'app-paypalform',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule,MatRadioModule],
   templateUrl: './paypalform.component.html',
   styleUrl: './paypalform.component.css'
 })
