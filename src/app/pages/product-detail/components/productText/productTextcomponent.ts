422123fb7fa1305011602ce89fcfdc8b779fe8fa
azadeh dehgan <azadeh69db@gmail.com>
2025-12-20 00:47:47 +0330

    ok

--- src/app/pages/product-detail/components/productText/productTextcomponent.ts
+++ src/app/pages/product-detail/components/productText/productTextcomponent.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import {MatTabsModule} from '@angular/material/tabs';
 @Component({
   selector: 'app-productText',
   standalone: true,
-  imports: [],
+  imports: [MatTabsModule],
   templateUrl: './productText.component.html',
   styleUrl: './productText.component.css'
 })
-export class newProductComponent {
+export class productTextComponent {
 
 }
--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -6,12 +6,15 @@ import { newProductComponent } from './components/newProduct/newProduct.componen
 import { productBoxComponent } from './components/productBox/productBox.component';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { FooterComponent } from "../../shared/footer/footer.component";
+import { productTextComponent } from './components/productText/productTextcomponent';
+
 @Component({
   selector: 'app-product-detail',
   standalone: true,
   imports: [categoryComponent, RouterLink,
     freeShipingComponent, newProductComponent,
-    productBoxComponent, HeaderComponent, FooterComponent],
+    productBoxComponent, HeaderComponent, FooterComponent
+  ,productTextComponent],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
