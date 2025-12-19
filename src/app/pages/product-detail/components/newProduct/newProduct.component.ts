c1a2c89a5aedef50528afa54464c1ed6c50696ff
azadeh dehgan <azadeh69db@gmail.com>
2025-12-19 23:00:08 +0330

    ok

--- src/app/pages/product-detail/components/newProduct/newProduct.component.ts
+++ src/app/pages/product-detail/components/newProduct/newProduct.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-newProduct',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule,MatButtonModule],
   templateUrl: './newProduct.component.html',
   styleUrl: './newProduct.component.css'
 })
--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { categoryComponent } from './components/category/category.component';
 import { RouterLink } from '@angular/router';
 import { freeShipingComponent } from './components/freeShiping/freeShiping.component';
+import { newProductComponent } from './components/newProduct/newProduct.component';
 @Component({
   selector: 'app-product-detail',
   standalone: true,
   imports: [categoryComponent,RouterLink
-    ,freeShipingComponent
+    ,freeShipingComponent,newProductComponent
   ],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
