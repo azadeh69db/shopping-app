e2de84179012e9797cf3ff187ef92234a5e2f434
azadeh dehgan <azadeh69db@gmail.com>
2025-12-19 23:38:22 +0330

    ok

--- src/app/pages/product-detail/components/productBox/productBox.component.ts
+++ src/app/pages/product-detail/components/productBox/productBox.component.ts
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './productBox.component.html',
   styleUrl: './productBox.component.css'
 })
-export class newProductComponent {
+export class productBoxComponent {
+count = 1;
+  min = 1;
+  max = 100;
 
+  increase() {
+    if (this.count < this.max) {
+      this.count++;
+    }
+  }
+
+  decrease() {
+    if (this.count > this.min) {
+      this.count--;
+    }
+  }
 }
--- src/app/pages/product-detail/product-detail.component.ts
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -3,11 +3,13 @@ import { categoryComponent } from './components/category/category.component';
 import { RouterLink } from '@angular/router';
 import { freeShipingComponent } from './components/freeShiping/freeShiping.component';
 import { newProductComponent } from './components/newProduct/newProduct.component';
+import { productBoxComponent } from './components/productBox/productBox.component';
 @Component({
   selector: 'app-product-detail',
   standalone: true,
   imports: [categoryComponent,RouterLink
     ,freeShipingComponent,newProductComponent
+    ,productBoxComponent
   ],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
