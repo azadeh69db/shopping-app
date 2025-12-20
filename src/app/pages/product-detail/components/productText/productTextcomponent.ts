754b972fc6cf78acf4fde453e4b4aadd861b0a49
azadeh dehgan <azadeh69db@gmail.com>
2025-12-20 14:47:45 +0330

    ok

--- src/app/pages/product-detail/components/productText/productTextcomponent.ts
+++ src/app/pages/product-detail/components/productText/productTextcomponent.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
+import {MatInputModule} from '@angular/material/input';
 @Component({
   selector: 'app-productText',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule,MatInputModule],
   templateUrl: './productText.component.html',
   styleUrl: './productText.component.css'
 })
